24c21b979707d198e6914c04a7345f420640afe7
Anatoli Malev <makcom@hotmail.co.uk>
2018-03-12 17:06:01 +0000

    Repository.SitefinityCMS fixed as well

--- DFC.Digital/DFC.Digital.Web.Sitefinity.JobProfileModule.UnitTests/Views/BauJobProfileSignPostViewTests.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.JobProfileModule.UnitTests/Views/BauJobProfileSignPostViewTests.cs
@@ -9,30 +9,30 @@ namespace DFC.Digital.Web.Sitefinity.JobProfileModule.UnitTests
 {
     public class BauJobProfileSignpostViewTests
     {
-        //[Theory]
-        //[InlineData("data")]
-        //[InlineData("test")]
+        [Theory]
+        [InlineData("data")]
+        [InlineData("test")]
 
-        //// Signposting on Beta JP page to point to specific BAU JP page
-        //public void DFC2223A1BAUJobProfileSignpostIndex(string contentdata)
-        //{
-        //    // Arrange
-        //    var indexView = new _MVC_Views_JobProfileBAUSignposting_Index_cshtml();
-        //    var signPostDummyVm = new BAUJobProfileSignpostViewModel
-        //    {
-        //        SignpostingHtml = contentdata
-        //    };
+        // Signposting on Beta JP page to point to specific BAU JP page
+        public void DFC2223A1BAUJobProfileSignpostIndex(string contentdata)
+        {
+            // Arrange
+            var indexView = new _MVC_Views_JobProfileBAUSignposting_Index_cshtml();
+            var signPostDummyVm = new BAUJobProfileSignpostViewModel
+            {
+                SignpostingHtml = contentdata
+            };
 
-        //    // Act
-        //    var htmlDom = indexView.RenderAsHtml(signPostDummyVm);
+            // Act
+            var htmlDom = indexView.RenderAsHtml(signPostDummyVm);
 
-        //    // Assert
-        //    GetContentData(htmlDom).Should().BeEquivalentTo(contentdata);
-        //}
+            // Assert
+            GetContentData(htmlDom).Should().BeEquivalentTo(contentdata);
+        }
 
-        //private string GetContentData(HtmlDocument htmlDom)
-        //{
-        //    return htmlDom.DocumentNode?.InnerText.Trim();
-        //}
+        private string GetContentData(HtmlDocument htmlDom)
+        {
+            return htmlDom.DocumentNode?.InnerText.Trim();
+        }
     }
 }

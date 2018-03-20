a8ac13bfb57e6115f90106ee060e05d691a73eb8
Ismail Lambat <lambat_15@hotmail.com>
2018-03-20 09:54:07 +0000

    Addition of Plugin project for TLS1.2 support on Load Tests Changes to Load tests to include plugin. Addition of data to the Profile Source Amending of Refuse collector on JCSource as this was failing

---
+++ DFC.Digital/DFC.Digital.TLS1.2PerformancePlugin/Tls12Plugin.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DFC.Digital.TLS12PerformancePlugin
+{
+    public class Tls12Plugin : WebTestPlugin
+    {
+        [Description("Enable or Disable the plugin functionality")]
+        [DefaultValue(true)]
+        public bool Enabled { get; set; }
+
+        public override void PreWebTest(object sender, PreWebTestEventArgs e)
+        {
+            base.PreWebTest(sender, e);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCB;
+
+
+            e.WebTest.AddCommentToResult(this.ToString() + " has made the following modification-> ServicePointManager.SecurityProtocol set to use Tlsv12 in WebTest Plugin.");
+        }
+        public static bool RemoteCertificateValidationCB(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            //If it is really important, validate the certificate issuer here.
+            //this will accept any certificate
+            return true;
+        }
+    }
+}

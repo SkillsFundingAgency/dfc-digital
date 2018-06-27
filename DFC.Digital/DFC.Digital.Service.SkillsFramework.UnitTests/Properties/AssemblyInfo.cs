7c581b1b56ea22599984602dd611ef266330bb88
Dinesh Tripatthi <dinesh.tripathi@aaditricorp.onmicrosoft.com>
2018-06-27 11:07:48 +0100

    WIP- WiT Business Rule Engine

---
+++ DFC.Digital/DFC.Digital.Service.SkillsFramework.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ( "DFC.Digital.Service.SkillsFramework.UnitTests" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "DFC.Digital.Service.SkillsFramework.UnitTests" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2018" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+[assembly: ComVisible ( false )]
+
+[assembly: Guid ( "7bdc1269-9e96-4595-a783-fa8b84c840f3" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
---
+++ DFC.Digital/DFC.Digital.Service.SkillsFramework.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DFC.Digital.Service.SkillsFramework.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1 ( )
+        {
+        }
+    }
+}

30ece58dae03d0f053d6b4e4023c71d2b00cd77e
trevorkapswarah <trevk15@yahoo.co.uk>
2018-07-05 17:36:45 +0100

    Story/DFC-3598 (BE & FE) deconstructed wyd in Sitefinity (#120)
    * WIP- What you will do
    
    * WIP - WYD
    
    * Added Unit Test for Controller and Views. Will ask Trevor to review it before pull request can be created.
    
    * updated model
    
    * Null checks on object
    
    * DataModel update
    
    * Model updated with correct setter
    
    * Dependency on BackEnd Sitefinity repository.  WIP
    
    * blank space removed.
    
    * Added dynamic content for Onet
    
    * Added nupkg
    
    * Removed Job Profile Keywords
    Added Digital Skills Level
    Updated version to 1.0.29.1
    
    * Added package file
    
    * Added unit test case for code coverage .
    
    * Integration with BE - Before unit tests
    
    * Integration of BE and FE , now to do unit tests
    
    * Added unit test.
    
    * Code cleanup
    
    * View tests done
    
    * WYD Repo converter changes
    
    * code coverage
    
    * Changes to add on
    
    * Fixing view
    
    * Code Clean up and unused code
    
    * Updated the view
    
    * Rolling back changes
    
    * Naming changes

---
+++ DFC.Digital/DFC.Digital.Repository.SitefinityCMS.UnitTests/Modules/WhatYouWillDoConverterTests.cs
@@ -0,0 +1,61 @@
+﻿using FakeItEasy;
+using System.Collections.Generic;
+using System.Linq;
+using Telerik.Sitefinity.DynamicModules.Model;
+using Telerik.Sitefinity.Model;
+using Xunit;
+
+namespace DFC.Digital.Repository.SitefinityCMS.Modules.Tests
+{
+    public class WhatYouWillDoConverterTests
+    {
+        private readonly IDynamicContentExtensions fakeDynamicContentExtensions;
+        private readonly DynamicContent fakeDynamicContentItem;
+
+        public WhatYouWillDoConverterTests()
+        {
+            fakeDynamicContentExtensions = A.Fake<IDynamicContentExtensions>();
+            fakeDynamicContentItem = A.Dummy<DynamicContent>();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ConvertFromTest(bool cadReady)
+        {
+            //Assign
+            SetupCalls(cadReady);
+            var whatYouWillDoConverter =
+                new WhatYouWillDoConverter(fakeDynamicContentExtensions);
+
+            //Act
+            whatYouWillDoConverter.ConvertFrom(fakeDynamicContentItem);
+
+            if (cadReady)
+            {
+                //Assert
+                A.CallTo(() => fakeDynamicContentExtensions.GetFieldValue<Lstring>(A<DynamicContent>._, A<string>._))
+                    .MustHaveHappened();
+                A.CallTo(() => fakeDynamicContentExtensions.GetRelatedItems(A<DynamicContent>._, A<string>._, A<int>._))
+                    .MustHaveHappened();
+            }
+            else
+            {
+                //Assert
+                A.CallTo(() => fakeDynamicContentExtensions.GetFieldValue<Lstring>(A<DynamicContent>._, A<string>._))
+                    .MustNotHaveHappened();
+            }
+        }
+
+        private void SetupCalls(bool cadReady)
+        {
+            A.CallTo(() => fakeDynamicContentExtensions.GetFieldValue<Lstring>(A<DynamicContent>._, A<string>._))
+                .Returns("test");
+
+            A.CallTo(() => fakeDynamicContentExtensions.GetFieldValue<bool>(A<DynamicContent>._, A<string>._))
+                .Returns(cadReady);
+            A.CallTo(() => fakeDynamicContentExtensions.GetRelatedItems(A<DynamicContent>._, A<string>._, A<int>._))
+                .Returns(new EnumerableQuery<DynamicContent>(new List<DynamicContent> { fakeDynamicContentItem }));
+        }
+    }
+}

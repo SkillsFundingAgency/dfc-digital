8fb755725694a0a885d17eb226debd922d716f42
Muthuramana <muthu@lightningcloudconsultancy.com>
2018-07-11 09:10:57 +0100

    v1.0.29.1 (#128)
    ### This release v1.0.29.1 (#128) contains the following changes
    
    - Release clean-up scripts -117  
    - DFC 3156 - create sitefinity add in to support o net (#119) 
    - DFC-3668 - Added the required API's to web service (#121) 
    - DFC-3598 - (BE & FE) deconstructed wyd in Sitefinity (#120)
    - DFC-3519 - Added JS SCSS and SVGs for JP feedback (#124 ), Added JS hidden JS visible classes to enable show hide (#127)

---
+++ DFC.Digital/DFC.Digital.Data/Model/WhatYouWillDo.cs
@@ -0,0 +1,17 @@
+﻿namespace DFC.Digital.Data.Model
+{
+    public class WhatYouWillDo
+    {
+        public bool IsCadReady { get; set; }
+
+        public string DailyTasks { get; set; }
+
+        public string Location { get; set; }
+
+        public string Uniform { get; set; }
+
+        public string Environment { get; set; }
+
+        public string Introduction { get; set; }
+    }
+}

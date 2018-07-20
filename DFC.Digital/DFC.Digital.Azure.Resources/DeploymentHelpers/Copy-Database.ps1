b2936843f078758500f51dfee7fc918e05bc57a6
Richard Lee <ric@dicci.co.uk>
2018-07-20 10:54:43 +0100

    Replaced depreciated Find-AzureRmResource with Get-AzureRmResource

--- DFC.Digital/DFC.Digital.Azure.Resources/DeploymentHelpers/Copy-Database.ps1
+++ DFC.Digital/DFC.Digital.Azure.Resources/DeploymentHelpers/Copy-Database.ps1
@@ -9,33 +9,36 @@ Param(
 )
 
 try {
-
-    # --- Extract short name from fqdn
-    $ServerName = $ServerName.Substring(0, $ServerName.IndexOf("."))
+    if ($ServerName.IndexOf(".") -ne -1) {
+        # --- Extract short name from fqdn
+        $ServerName = $ServerName.Substring(0, $ServerName.IndexOf("."))
+    }
 
     # --- Retrieve server resource
     Write-Host "Searching for server resource $($ServerName)"
-    $ServerResource = Find-AzureRmResource -ResourceNameEquals $ServerName -ResourceType "Microsoft.Sql/servers"
+    #$ServerResource = Find-AzureRmResource -ResourceNameEquals $ServerName -ResourceType "Microsoft.Sql/servers"
+    $ServerResource = Get-AzureRmResource -Name $ServerName -ResourceType "Microsoft.Sql/servers"
     if (!$ServerResource) {
         throw "Could not find server resource $($ServerName)"
     }
 
     # --- Extract the build number if it is not provided
-    if (!$PSBoundParameters.ContainsKey("ReleaseNumber")){
+    if (!$PSBoundParameters.ContainsKey("ReleaseNumber")) {
         $ReleaseNumber = $ENV:RELEASE_RELEASENAME.Split("-")[0]
     }
 
     # --- Get the database version that is currently being used in production
     Write-Host "Searching for app service $AppServiceName"
-    $AppServiceResource = Find-AzureRmResource -ResourceNameEquals $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
-    if (!$AppServiceResource){
+    #$AppServiceResource = Find-AzureRmResource -ResourceNameEquals $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+    $AppServiceResource = Get-AzureRmResource -Name $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+    if (!$AppServiceResource) {
         throw "Could not find app service with name $AppServiceName"
     }
 
     # --- Resolve app service and retrieve app settings
     $AppService = Get-AzureRmWebApp -ResourceGroupName $AppServiceResource.ResourceGroupName -Name $AppServiceName
     $DatabaseVersionAppSetting = ($AppService.SiteConfig.AppSettings | Where-Object {$_.Name -eq "DatabaseVersion"}).Value
-    if (!$DatabaseVersionAppSetting){
+    if (!$DatabaseVersionAppSetting) {
         throw "Could not determine current database version from DatabaseVersion setting"
     }
 
@@ -43,7 +46,8 @@ try {
     $FirstRun = [Regex]::Match($DatabaseVersionAppSetting, "(?i)R(?-i)[0-9]").Success
     if ($FirstRun -eq "True") {
         $DatabaseName = $DatabaseVersionAppSetting.Substring(0, $DatabaseVersionAppSetting.LastIndexOf("-"))
-    } else {
+    }
+    else {
         $DatabaseName = $DatabaseVersionAppSetting
     }
 
@@ -66,7 +70,8 @@ try {
         $ElapsedTime = $StopWatch.Elapsed.ToString('hh\:mm\:ss')
         Write-Host "Database copy completed in $ElapsedTime"
    
-    } else {
+    }
+    else {
         Write-Host "A database copy with name $CopyDatabaseName exists. Skipping"
     }
 

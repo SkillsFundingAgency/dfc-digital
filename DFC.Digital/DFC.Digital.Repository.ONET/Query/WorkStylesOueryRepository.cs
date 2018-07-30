4622546238be851249b273fb428bba3e4dd7b042
gurney <Gurney@as-tech.co.uk>
2018-07-30 15:29:55 +0100

    Added combinations and suppressions repository

---
+++ DFC.Digital/DFC.Digital.Repository.ONET/Query/WorkStylesOueryRepository.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DFC.Digital.Data.Interfaces;
+using DFC.Digital.Data.Model;
+using DFC.Digital.Repository.ONET.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFC.Digital.Repository.ONET.Query
+{
+
+    public class WorkStylesOueryRepository : ISkillsRepository
+    {
+        private readonly OnetSkillsFramework onetDbContext;
+
+        public WorkStylesOueryRepository(OnetSkillsFramework onetDbContext)
+        {
+            this.onetDbContext = onetDbContext;
+        }
+
+        public IQueryable<OnetAttribute> GetSkillsForONetOccupationCode(string oNetOccupationCode)
+        {
+            var attributes = from workStyle in onetDbContext.work_styles
+                                    where workStyle.recommend_suppress != "Y"
+                                        && workStyle.onetsoc_code == oNetOccupationCode
+                                    select new OnetAttribute
+                                    {
+                                        OnetOccupationalCode = workStyle.onetsoc_code,
+                                        Id = workStyle.element_id,
+                                        Type = AttributeType.WorkStyle,
+                                        Score = workStyle.data_value
+                                    };
+            return attributes;
+        }
+    }
+}

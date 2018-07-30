f23449ef8fe7795dc1e9bc9b42208915a6ff1915
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-07-30 15:16:18 +0100

    WIP fix

--- DFC.Digital/DFC.Digital.Service.SkillsFramework/SkillsFrameworkService.cs
+++ DFC.Digital/DFC.Digital.Service.SkillsFramework/SkillsFrameworkService.cs
@@ -11,18 +11,23 @@ namespace DFC.Digital.Service.SkillsFramework
     public class SkillsFrameworkService : ISkillsFrameworkService
     {
         private readonly IApplicationLogger logger;
-        private readonly IQueryRepository<SocCode> socRepository;
-        private readonly IQueryRepository<DigitalSkill> digitalSkillRepository;
-        private readonly IQueryRepository<FrameworkSkill> translationRepository;
+        private readonly IRepository<SocCode> socRepository;
 
+        private readonly IRepository<DigitalSkill> digitalSkillRepository;
         private readonly ISkillFrameworkBusinessRuleEngine skillsBusinessRuleEngine;
-   
+        private readonly IRelatedSkillsMappingRepository skillsMappingRepository;
+
+        //private readonly IRepository<RelatedSkillMapping> skillsMappingRepository;
+        private readonly IRepository<WhatItTakesSkill> skillsRepository;
+
         public SkillsFrameworkService(
             IApplicationLogger logger,
-            IQueryRepository<SocCode> socRepository,
-            IQueryRepository<DigitalSkill> digitalSkillRepository,
-            IQueryRepository<FrameworkSkill> translationRepository,
-            ISkillFrameworkBusinessRuleEngine skillsBusinessRuleEngine)
+            IRepository<SocCode> socRepository,
+            IRepository<DigitalSkill> digitalSkillRepository,
+            ISkillFrameworkBusinessRuleEngine skillsBusinessRuleEngine,
+            //IRepository<RelatedSkillMapping> skillsMappingRepository,
+            IRelatedSkillsMappingRepository skillsMappingRepository,
+            IRepository<WhatItTakesSkill> skillsRepository)
         {
             this.logger = logger;
             this.socRepository = socRepository;
@@ -46,35 +51,34 @@ namespace DFC.Digital.Service.SkillsFramework
         public DigitalSkillsLevel GetDigitalSkillLevel(string onetOccupationalCode)
         {
             var digitalSkill = digitalSkillRepository.GetById(onetOccupationalCode);
-            var rank= skillsBusinessRuleEngine.GetDigitalSkillsLevel(digitalSkill.ApplicationCount);
-            return rank;
+            return skillsBusinessRuleEngine.GetDigitalSkillsLevel(digitalSkill.ApplicationCount);
         }
 
         public IEnumerable<OnetAttribute> GetRelatedSkillMapping(string onetOccupationalCode)
         {
 
-            //Get All raw attributes linked to occ code from the repository (Skill, knowledge, work styles, ablities)
-           var attributes = skillsBusinessRuleEngine.GetAllRawOnetSkillsForOccupation(onetOccupationalCode);
+           // //Get All raw attributes linked to occ code from the repository (Skill, knowledge, work styles, ablities)
+           //var attributes = skillsBusinessRuleEngine.GetAllRawOnetSkillsForOccupation(onetOccupationalCode);
 
-           //attributes =  skillsBusinessRuleEngine.RemoveDFCSuppressions(attributes);
+            attributes =  skillsBusinessRuleEngine.RemoveDFCSuppressions(attributes);
 
 
             //Average out the skill thats have LV and LM scales
-            //attributes = skillsBusinessRuleEngine.AverageOutScoreScales(attributes);
+            attributes = skillsBusinessRuleEngine.AverageOutScoreScales(attributes);
 
-            //attributes = skillsBusinessRuleEngine.MoveBottomLevelAttributesUpOneLevel(attributes);
+            attributes = skillsBusinessRuleEngine.MoveBottomLevelAttributesUpOneLevel(attributes);
 
-            //attributes =  skillsBusinessRuleEngine.RemoveDuplicateAttributes(attributes);
+            attributes =  skillsBusinessRuleEngine.RemoveDuplicateAttributes(attributes);
 
-            //attributes =  skillsBusinessRuleEngine.BoostMathsSkills(attributes);
+            attributes =  skillsBusinessRuleEngine.BoostMathsSkills(attributes);
 
-            //attributes =  skillsBusinessRuleEngine.CombineSimilarAttributes(attributes);
+            attributes =  skillsBusinessRuleEngine.CombineSimilarAttributes(attributes);
 
-            //attributes = skillsBusinessRuleEngine.SelectFinalAttributes(attributes);
+            attributes = skillsBusinessRuleEngine.SelectFinalAttributes(attributes);
 
-            //attributes =  skillsBusinessRuleEngine.AddTitlesToAttributes(attributes);
+            attributes =  skillsBusinessRuleEngine.AddTitlesToAttributes(attributes);
 
-            return skillsBusinessRuleEngine.AddTitlesToAttributes(attributes);
+            return default;
         }
 
 

54b7814f9baa1cc5f758e5b42cd3b4088ebbcd15
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-07-30 09:26:54 +0100

    starting unit tests

---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.JobProfileModule.UnitTests/Services/FormatContentServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DFC.Digital.Web.Sitefinity.JobProfileModule.UnitTests
+{
+    public class FormatContentServiceTests
+    {
+        [Theory]
+        [InlineData("intro", 1, "and", "intro item")]
+        public void GetParagraphTextTest(string leadingText, int numberOfItems, string conjunction, string expected)
+        {
+            // Arrange
+           var formatContentService = new FormatContentService();
+            var list = GetRelatedItems(numberOfItems);
+
+            // Act
+             var result = formatContentService.GetParagraphText(leadingText, list, conjunction);
+
+            // Assert
+            result.Should().Be(expected);
+
+        }
+
+        private IEnumerable<string> GetRelatedItems(int numberOfItems)
+        {
+            for (int n = 0; n < numberOfItems; n++)
+            {
+              yield return "item";
+            }
+        }
+    }
+}

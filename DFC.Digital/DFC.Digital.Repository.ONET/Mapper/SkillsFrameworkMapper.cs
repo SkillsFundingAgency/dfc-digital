3e0f6cc27d452b17a9e292f96e711a8bd7609ca0
Dinesh Tripatthi <dinesh.tripathi@aaditricorp.onmicrosoft.com>
2018-08-01 17:28:30 +0100

    for merge

--- DFC.Digital/DFC.Digital.Repository.ONET/Mapper/SkillsFrameworkMapper.cs
+++ DFC.Digital/DFC.Digital.Repository.ONET/Mapper/SkillsFrameworkMapper.cs
@@ -8,15 +8,15 @@ namespace DFC.Digital.Repository.ONET.Mapper
     {
         public SkillsFrameworkMapper()
         {
-           
-            CreateMap<DFC_GDSTranlations, WhatItTakesSkill>()
+
+                 CreateMap<DFC_GDSTranlations, WhatItTakesSkill>()
                 .ForMember(d => d.Description, m => m.MapFrom(s => s.translation.Trim()))
                 .ForMember(d => d.Title, m => m.MapFrom(d => d.onet_element_id))
                 .ReverseMap()
                 .ForMember(s => s.translation, m => m.MapFrom(d => d.Description))
                 .ForMember(s => s.onet_element_id, m => m.MapFrom(d => d.Title));
 
-                 CreateMap<DFC_GDSTranlations, FrameWorkSkill>()
+                 CreateMap<DFC_GDSTranlations, FrameworkSkill>()
                 .ForMember(d => d.Description, m => m.MapFrom(s => s.translation.Trim()))
                 .ForMember(d => d.ONetElementId, m => m.MapFrom(d => d.onet_element_id))
                 .ReverseMap()

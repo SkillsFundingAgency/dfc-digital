5db2b03ba2f499c13305ab3bbc5d3c8060aa7e0e
Muthuramana <muthu@lightningcloudconsultancy.com>
2018-08-20 11:28:38 +0100

    Release v1.0.32.3 (#149)
    # Patch Release v1.0.32.3 (#149)
    
    - DFC-4143 - Fix for search re-index

--- DFC.Digital/DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests/SearchIndexEnhancers/JobProfileIndexEnhancerTests.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests/SearchIndexEnhancers/JobProfileIndexEnhancerTests.cs
@@ -13,11 +13,9 @@ namespace DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests
     public class JobProfileIndexEnhancerTests
     {
         [Theory]
-        [InlineData(false, false, 1000, 2000)]
-        [InlineData(false, true, 1000, 2000)]
-        [InlineData(true, false, 10, 20)]
-        [InlineData(true, true, 10, 20)]
-        public async Task GetSalaryRangeAsyncTestAsync(bool isSalaryOverriden, bool isPublishing, double salaryStarterExpected, double salaryExperiencedExpected)
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task GetSalaryRangeAsyncTestAsync(bool isPublishing)
         {
             var fakeJobProfileRepo = A.Fake<IJobProfileRepository>();
             var fakeJobProfileCategoryRepo = A.Fake<IJobProfileCategoryRepository>();
@@ -26,9 +24,6 @@ namespace DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests
             var dummyJobProfileIndex = A.Dummy<JobProfileIndex>();
             var dummyJobProfile = new JobProfile
             {
-                IsLMISalaryFeedOverriden = isSalaryOverriden,
-                SalaryStarter = (decimal)salaryStarterExpected,
-                SalaryExperienced = (decimal)salaryExperiencedExpected,
                 SOCCode = nameof(JobProfile.SOCCode)
             };
             var dummySalary = new JobProfileSalary
@@ -51,23 +46,12 @@ namespace DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests
 
             await enhancer.PopulateSalary();
 
-            dummyJobProfileIndex.SalaryExperienced.Should().Be(salaryExperiencedExpected);
-            dummyJobProfileIndex.SalaryStarter.Should().Be(salaryStarterExpected);
             A.CallTo(() => fakeJobProfileRepo.GetByUrlNameForSearchIndex(A<string>._, isPublishing)).MustHaveHappened();
             A.CallTo(() => fakeJobProfileRepo.GetByUrlName(A<string>._)).MustNotHaveHappened();
 
-            if (isSalaryOverriden)
-            {
-                A.CallTo(() => salaryService.GetSalaryBySocAsync(A<string>._)).MustNotHaveHappened();
-                A.CallTo(() => salaryCalculator.GetStarterSalary(A<JobProfileSalary>._)).MustNotHaveHappened();
-                A.CallTo(() => salaryCalculator.GetExperiencedSalary(A<JobProfileSalary>._)).MustNotHaveHappened();
-            }
-            else
-            {
-                A.CallTo(() => salaryService.GetSalaryBySocAsync(A<string>._)).MustHaveHappened();
-                A.CallTo(() => salaryCalculator.GetStarterSalary(A<JobProfileSalary>._)).MustHaveHappened();
-                A.CallTo(() => salaryCalculator.GetExperiencedSalary(A<JobProfileSalary>._)).MustHaveHappened();
-            }
+            A.CallTo(() => salaryService.GetSalaryBySocAsync(A<string>._)).MustHaveHappened();
+            A.CallTo(() => salaryCalculator.GetStarterSalary(A<JobProfileSalary>._)).MustHaveHappened();
+            A.CallTo(() => salaryCalculator.GetExperiencedSalary(A<JobProfileSalary>._)).MustHaveHappened();
         }
 
         [Theory]
@@ -80,16 +64,7 @@ namespace DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests
             var salaryService = A.Fake<ISalaryService>();
             var salaryCalculator = A.Fake<ISalaryCalculator>();
             var dummyJobProfileIndex = A.Dummy<JobProfileIndex>();
-            var dummyJobProfile = new JobProfile
-            {
-                RelatedInterests = new[] { "1|one", "2|two" }.AsQueryable(),
-                RelatedEnablers = new[] { "1|one", "2|two" }.AsQueryable(),
-                RelatedEntryQualifications = new[] { "1|one", "2|two" }.AsQueryable(),
-                RelatedTrainingRoutes = new[] { "1|one", "2|two" }.AsQueryable(),
-                RelatedPreferredTaskTypes = new[] { "1|one", "2|two" }.AsQueryable(),
-                RelatedJobAreas = new[] { "1|one", "2|two" }.AsQueryable()
-            };
-
+            var dummyJobProfile = A.Dummy<JobProfile>();
             var dummyCategories = new List<JobProfileCategory>
             {
                 new JobProfileCategory
@@ -120,12 +95,6 @@ namespace DFC.Digital.Web.Sitefinity.DfcSearchModule.UnitTests
             A.CallTo(() => fakeJobProfileCategoryRepo.GetByIds(A<IList<Guid>>._)).MustHaveHappened();
 
             dummyJobProfileIndex.JobProfileCategoriesWithUrl.Should().BeEquivalentTo(expectedCategories);
-            dummyJobProfileIndex.Interests.Should().BeEquivalentTo(dummyJobProfile.RelatedInterests);
-            dummyJobProfileIndex.Enablers.Should().BeEquivalentTo(dummyJobProfile.RelatedEnablers);
-            dummyJobProfileIndex.EntryQualifications.Should().BeEquivalentTo(dummyJobProfile.RelatedEntryQualifications);
-            dummyJobProfileIndex.TrainingRoutes.Should().BeEquivalentTo(dummyJobProfile.RelatedTrainingRoutes);
-            dummyJobProfileIndex.JobAreas.Should().BeEquivalentTo(dummyJobProfile.RelatedJobAreas);
-            dummyJobProfileIndex.PreferredTaskTypes.Should().BeEquivalentTo(dummyJobProfile.RelatedPreferredTaskTypes);
         }
     }
 }

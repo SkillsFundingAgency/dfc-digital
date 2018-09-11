c3f0e4682a8390ec29838ccae64e32123854ba6f
Muthuramana <muthu@lightningcloudconsultancy.com>
2018-09-11 19:19:35 +0100

    Release v1.0.34.2 (#186)
    This Release v1.0.34.2 (#186) contains the following changes
    
    - DFC-4358 - Removed un-used GA line that causing exception (#185)
    - DFC-4356 - Replacing Find a career with Explore careers (#184)
    - DFC-4303 - Accessibility fix - salary range description code change with property (#182)
    - Added the IncludeInSitemap property mapping to Data Import Tool (#181)
    - Accessibility fix - CSS update for arrow screen reader issue (#180)
    - DFC-4298 - Accessibility fix - enable hidden text (#179)
    - Accessibility fix - Updated search page to remove fieldset and quotation marks around query (#178)
    - DFC-4303 - Accessibility fix - salary range description (#177)
    - DFC-4291 improve code coverage on solution (#176)

---
+++ DFC.Digital/DFC.Digital.Service.SkillsFramework.UnitTests/InMemoryReportAuditRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace DFC.Digital.Service.SkillsFramework.UnitTests
+{
+    public class InMemoryReportAuditRepositoryTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void GetAllAuditRecordsTests(bool isEmpty)
+        {
+            var objectToTest = new InMemoryReportAuditRepository();
+
+            if (isEmpty)
+            {
+                objectToTest.CreateAudit("test", "dummy");
+            }
+        
+           objectToTest.GetAllAuditRecords().Keys.Any().Should().Be(isEmpty);
+            
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CreateAuditTests(bool categoryExists)
+        {
+            var objectToTest = new InMemoryReportAuditRepository();
+
+            if (categoryExists)
+            {
+                objectToTest.CreateAudit("test", "dummy");
+            }
+
+            objectToTest.CreateAudit("test", "test");
+
+             objectToTest.GetAllAuditRecords()["test"].Should().Contain("test");
+            
+        }
+    }
+}

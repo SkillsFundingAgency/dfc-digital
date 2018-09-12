cde8cb8c6ef9ba07cb7f39fc8c763a2127095e4c
Richard Lee <github@dicci.co.uk>
2018-09-12 18:02:27 +0100

    Change from Find-AzureRmResource to Get-AzureRmResource for Azure PS 6 (#183)

--- DFC.Digital/DFC.Digital.Azure.Resources/DeploymentHelpers/Set-SearchIndexName.ps1
+++ DFC.Digital/DFC.Digital.Azure.Resources/DeploymentHelpers/Set-SearchIndexName.ps1
@@ -7,10 +7,15 @@ Param(
 )
 
 try {
+    $azurepsver = ((Get-Module AzureRM -ListAvailable | Sort-Object { $_.Version.Major } -Descending).Version.Major)[0]
 
     # --- Get the database version that is currently being used in production
     Write-Host "Searching for app service $AppServiceName"
-    $AppServiceResource = Find-AzureRmResource -ResourceNameEquals $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+    if ($azurepsver -lt 6) {
+        $AppServiceResource = Find-AzureRmResource -ResourceNameEquals $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+    } else {
+        $AppServiceResource = Get-AzureRmResource -Name $AppServiceName -ResourceType "Microsoft.Web/sites" -ErrorAction SilentlyContinue
+    }
     if (!$AppServiceResource){
         throw "Could not find app service with name $AppServiceName"
     }

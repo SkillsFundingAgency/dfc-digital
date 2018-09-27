42dbf3a393ecc60bc9881db64459de3b273acec2
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-09-27 10:58:43 +0100

    Ready for PR

--- DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Properties/AssemblyInfo.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Properties/AssemblyInfo.cs
@@ -35,5 +35,7 @@ using System.Web;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: PreApplicationStartMethod(typeof(Startup), "Install")]
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
+
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]

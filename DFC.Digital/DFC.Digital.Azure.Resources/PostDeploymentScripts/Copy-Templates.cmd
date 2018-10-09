c99ac346e9327d06f018dfa0ee5680bc60d6ccd9
Richard Lee <ric@dicci.co.uk>
2018-10-09 23:41:40 +0100

    Changed to shell script

---
+++ DFC.Digital/DFC.Digital.Azure.Resources/PostDeploymentScripts/Copy-Templates.cmd
@@ -0,0 +1,2 @@
+COPY %WEBROOT_PATH%\sitemap.xml.template %WEBROOT_PATH%\sitemap.xml
+COPY %WEBROOT_PATH%\robots.txt.template %WEBROOT_PATH%\robots.txt
--- DFC.Digital/DFC.Digital.Azure.Resources/PostDeploymentScripts/Copy-Templates.ps1
+++
@@ -1,1 +0,0 @@
-Write-Output "Running Copy-Templates"

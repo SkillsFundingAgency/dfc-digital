a185b8a37cc400b741bd021e9d7f187c8c950060
Richard Lee <ric@dicci.co.uk>
2018-10-10 09:47:25 +0100

    Fixed file regex for robots.txt and implemented renaming based on environment

--- DFC.Digital/DFC.Digital.Azure.Resources/PostDeploymentScripts/Copy-Templates.cmd
+++ DFC.Digital/DFC.Digital.Azure.Resources/PostDeploymentScripts/Copy-Templates.cmd
@@ -1,2 +1,11 @@
-COPY %WEBROOT_PATH%\sitemap.xml.template %WEBROOT_PATH%\sitemap.xml
-COPY %WEBROOT_PATH%\robots.txt.template %WEBROOT_PATH%\robots.txt
+RENAME %WEBROOT_PATH%\sitemap.xml.template sitemap.xml
+IF "%APPSETTING_ENVIRONMENT%" EQU "PRD" (
+    ECHO Renaming prod robots.txt 
+    RENAME %WEBROOT_PATH%\robots.prod.txt.template robots.txt
+    DEL %WEBROOT_PATH%\robots.txt.template
+)
+IF "%APPSETTING_ENVIRONMENT%" NEQ "PRD" (
+    ECHO Renaming non-prod robots.txt 
+    RENAME %WEBROOT_PATH%\robots.txt.template robots.txt
+    DEL %WEBROOT_PATH%\robots.prod.txt.template
+)

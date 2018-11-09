47b91f21f567e89dce4ef06b9bc7963fb9b8a4f0
Trevor Kapswarah <trevk15@yahoo.co.uk>
2018-11-09 12:09:16 +0000

    Code Review changes

--- DFC.Digital/DFC.Digital.Repository.SitefinityCMS/CMSExtensions/UserRepository.cs
+++ DFC.Digital/DFC.Digital.Repository.SitefinityCMS/CMSExtensions/UserRepository.cs
@@ -6,22 +6,51 @@ using Telerik.Sitefinity.Security.Model;
 namespace DFC.Digital.Repository.SitefinityCMS.CMSExtensions
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository, IDisposable
     {
+        private UserManager userManager;
+        private UserProfileManager userProfileManager;
+
+        public UserRepository()
+        {
+            userManager = UserManager.GetManager();
+            userProfileManager = UserProfileManager.GetManager();
+        }
+
         public string GetUserNameById(Guid id)
         {
-            //CodeReview: Do we have to get the manager for every single item?
-            var userManager = UserManager.GetManager();
             var user = userManager.GetUser(id);
             if (user != null)
             {
-                //CodeReview: Do we have to get the manager for every single item?
-                var profileManager = UserProfileManager.GetManager();
-                var profile = profileManager.GetUserProfile<SitefinityProfile>(user);
+                var profile = userProfileManager.GetUserProfile<SitefinityProfile>(user);
                 return profile.Nickname ?? $"{profile.FirstName} {profile.LastName}";
             }
 
             return "User not found";
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (userManager != null)
+                {
+                    userManager.Dispose();
+                    userManager = null;
+                }
+
+                if (userProfileManager != null)
+                {
+                    userProfileManager.Dispose();
+                    userProfileManager = null;
+                }
+            }
         }
     }
 }

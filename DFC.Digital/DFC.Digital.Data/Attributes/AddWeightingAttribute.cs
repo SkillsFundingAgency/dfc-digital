91196067b8fc528ce344754dff82b7ba54470e47
Gurnaik-Randhawa <Gurnaik-Randhawa@users.noreply.github.com>
2018-11-22 09:33:25 +0000

    Story/dfc 5220 search for exact match title should be first (#232)
    * Added scoring profile to ensure index
    
    * Added creation of scoring profile for field weightings
    
    * Added weighting builder to tests
    
    * renamed attribute

---
+++ DFC.Digital/DFC.Digital.Data/Attributes/AddWeightingAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFC.Digital.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class AddWeightingAttribute : Attribute
+    {
+        public AddWeightingAttribute(double weighting)
+        {
+            this.Weighting = weighting;
+        }
+
+        public double Weighting { get; private set; }
+    }
+}

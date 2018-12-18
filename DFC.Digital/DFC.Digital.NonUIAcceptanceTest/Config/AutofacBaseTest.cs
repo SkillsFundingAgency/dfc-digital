7407de65b7ea20baed9378b52c1898f80353566a
Muthuramana <muthu@lightningcloudconsultancy.com>
2018-12-18 11:10:19 +0000

    Release v1.1.4.1 (#254)
    ### This release v1.1.4.1 (#254) contains the following changes
    - #246 DFC-5740 - If search term exactly matches one of the alternative titles, then that profile should be displayed as first in the search results.
    - #249 DFC-5562 - Self referencing canonical tags

---
+++ DFC.Digital/DFC.Digital.NonUIAcceptanceTest/Config/AutofacBaseTest.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+
+namespace DFC.Digital.NonUIAcceptanceTest
+{
+    public class AutofacBaseTest<TModule> : IDisposable
+        where TModule : IModule, new()
+    {
+        private IContainer container;
+
+        public AutofacBaseTest()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TModule());
+            builder.RegisterModule<AutofacAcceptanceTestModule>();
+            container = builder.Build();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (container != null)
+                {
+                    container.Dispose();
+                }
+            }
+        }
+
+        protected TEntity Resolve<TEntity>()
+        {
+            return container.Resolve<TEntity>();
+        }
+    }
+}

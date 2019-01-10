f3d8603b1437c712ac81bc5e82f53d80b3d3fa3f
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-01-10 21:39:18 +0000

    Ready for PR

--- DFC.Digital/DFC.Digital.Repository.SitefinityCMS/Modules/RecycleBinRepository.cs
+++ DFC.Digital/DFC.Digital.Repository.SitefinityCMS/Modules/RecycleBinRepository.cs
@@ -1,63 +1,49 @@
 ﻿using DFC.Digital.Data.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using Telerik.Sitefinity.Configuration;
-using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.DynamicModules;
 using Telerik.Sitefinity.RecycleBin;
 using Telerik.Sitefinity.Services;
+using Telerik.Sitefinity.SitefinityExceptions;
 using Telerik.Sitefinity.Utilities.TypeConverters;
 
 namespace DFC.Digital.Repository.SitefinityCMS.Modules
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    public class RecycleBinRepository : IRecyleBinRepository, IDisposable
+    public class RecycleBinRepository : IRecyleBinRepository
     {
         private const string ApprenticeVacancyDeleteTypeName = "Telerik.Sitefinity.DynamicTypes.Model.JobProfile.ApprenticeshipVacancy";
-        private IRecycleBinManager recycleBinItemsManager;
-
-        public RecycleBinRepository()
-        {
-            recycleBinItemsManager = RecycleBinManagerFactory.GetManager();
-        }
 
         public void DeleteVacanciesPermanently(int itemCount)
         {
-            SystemManager.RunWithElevatedPrivilege(d =>
+            using (var recycleBinItemsManager = RecycleBinManagerFactory.GetManager())
             {
-                var recycleBinItems = recycleBinItemsManager.GetRecycleBinItems().Where(di => di.DeletedItemTypeName.Equals(ApprenticeVacancyDeleteTypeName)).Take(itemCount).ToList();
-
-                var providerName = DynamicModuleManager.GetDefaultProviderName(DynamicTypes.JobProfileModuleName);
-                var dynamicModuleManager = DynamicModuleManager.GetManager(providerName);
-                var dynamicModuleContentType = TypeResolutionService.ResolveType(ApprenticeVacancyDeleteTypeName);
-                foreach (var recycleBinItem in recycleBinItems)
+                SystemManager.RunWithElevatedPrivilege(d =>
                 {
-                    var dataItem =
-                            dynamicModuleManager.GetItem(dynamicModuleContentType, recycleBinItem.DeletedItemId) as IRecyclableDataItem;
-                    dynamicModuleManager.RecycleBin.PermanentlyDeleteFromRecycleBin(dataItem);
-                    dynamicModuleManager.SaveChanges();
-                }
-            });
+                    var recycleBinItems = recycleBinItemsManager.GetRecycleBinItems()
+                        .Where(di => di.DeletedItemTypeName.Equals(ApprenticeVacancyDeleteTypeName)).Take(itemCount).ToList();
+                    var providerName = DynamicModuleManager.GetDefaultProviderName(DynamicTypes.JobProfileModuleName);
 
-            recycleBinItemsManager.SaveChanges();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                if (recycleBinItemsManager != null)
-                {
-                    recycleBinItemsManager.Dispose();
-                    recycleBinItemsManager = null;
-                }
+                    using (var dynamicModuleManager = DynamicModuleManager.GetManager(providerName))
+                    {
+                        var dynamicModuleContentType = TypeResolutionService.ResolveType(ApprenticeVacancyDeleteTypeName);
+                        foreach (var recycleBinDataItem in recycleBinItems)
+                        {
+                            try
+                            {
+                                var dataItem =
+                                    dynamicModuleManager.GetItem(dynamicModuleContentType, recycleBinDataItem.DeletedItemId) as
+                                        IRecyclableDataItem;
+                                dynamicModuleManager.RecycleBin.PermanentlyDeleteFromRecycleBin(dataItem);
+                                dynamicModuleManager.SaveChanges();
+                            }
+                            catch (ItemNotFoundException)
+                            {
+                                recycleBinItemsManager.Delete(recycleBinDataItem);
+                            }
+                        }
+                    }
+                });
+                recycleBinItemsManager.SaveChanges();
             }
         }
     }

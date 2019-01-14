859264ed8b597fb84f7534e74e454e015cf7758c
Muthuramana Kandasamy <muthu@lightningcloudconsultancy.com>
2019-01-14 17:27:44 +0000

    tested

---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.WebApiTests/Mvc/Controllers/AVFeedRecycleBinControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using DFC.Digital.Web.Sitefinity.WebApi.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DFC.Digital.Data.Interfaces;
+using FakeItEasy;
+using FluentAssertions;
+using System.Web.Http.Results;
+using System.Net;
+
+namespace DFC.Digital.Web.Sitefinity.WebApi.Mvc.Controllers.Tests
+{
+    public class AVFeedRecycleBinControllerTests
+    {
+        [Theory]
+        [InlineData(false, HttpStatusCode.PartialContent)]
+        [InlineData(true, HttpStatusCode.OK)]
+        public void DeleteTest(bool isLastPage, HttpStatusCode expected)
+        {
+            var fakeRecycleBinRepo = A.Fake<IRecycleBinRepository>();
+            var api = new AVFeedRecycleBinController(fakeRecycleBinRepo);
+            A.CallTo(() => fakeRecycleBinRepo.DeleteVacanciesPermanently(A<int>._)).Returns(isLastPage);
+
+            var result = api.Delete(5);
+
+            // Assert
+            result.Should().BeOfType<StatusCodeResult>();
+
+            ((StatusCodeResult)result).StatusCode.Should().Be(expected);
+            A.CallTo(() => fakeRecycleBinRepo.DeleteVacanciesPermanently(A<int>.That.IsEqualTo(5))).MustHaveHappenedOnceExactly();
+        }
+    }
+}

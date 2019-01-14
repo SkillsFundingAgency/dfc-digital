4f47bd9d2429d426bd874efc3d6a39164b65b2d1
Muthuramana Kandasamy <muthu@lightningcloudconsultancy.com>
2019-01-14 21:21:00 +0000

    update

--- DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Config/AutofacContainerFactory.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Config/AutofacContainerFactory.cs
@@ -13,37 +13,6 @@ namespace DFC.Digital.Web.Sitefinity.Core
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public class AutofacContainerFactory : FrontendControllerFactory
     {
-        public override IController CreateController(RequestContext requestContext, string controllerName)
-        {
-            return base.CreateController(requestContext, controllerName);
-        }
-
-        public override void ReleaseController(IController controller)
-        {
-            base.ReleaseController(controller);
-        }
-
-        public override string ResolveControllerName(MvcProxyBase proxy)
-        {
-            return base.ResolveControllerName(proxy);
-        }
-
-        public override string ResolveControllerName(Type proxyType)
-        {
-            return base.ResolveControllerName(proxyType);
-        }
-
-        public override Type ResolveControllerType(string controllerName)
-        {
-            var something = base.ResolveControllerType(controllerName);
-            return something;
-        }
-
-        protected override SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, Type controllerType)
-        {
-            return base.GetControllerSessionBehavior(requestContext, controllerType);
-        }
-
         protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
         {
             if (controllerType == null)
--- DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Startup.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Startup.cs
@@ -115,8 +115,6 @@ namespace DFC.Digital.Web.Sitefinity.Core
 
             routes.MapRoute("serviceStatus", "health/{controller}/{action}", new { controller = "ServiceStatus", action = "Index", id = string.Empty });
             routes.MapRoute("restartsitefinity", "restartsitefinity/{controller}/{action}", new { controller = "AdminPanel", action = "RestartSitefinity", id = string.Empty });
-
-            //routes.MapRoute("dfcapi", "dfcapi/{controller}/{id}", new { id = RouteParameter.Optional });
         }
     }
 }

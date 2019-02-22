532920c7fb9dfb03f242c824c644f790acf13021
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-02-22 16:57:36 +0000

    Course Details page

---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.CourseModule/Mvc/Controllers/CourseDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using DFC.Digital.Core;
+using DFC.Digital.Web.Core;
+using DFC.Digital.Web.Sitefinity.Core;
+using Telerik.Sitefinity.Mvc;
+
+namespace DFC.Digital.Web.Sitefinity.CourseModule.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "CourseDetails", Title = "Course Details", SectionName = SitefinityConstants.CustomCoursesSection)]
+    public class CourseDetailsController : BaseDfcController
+    {
+        // GET: CourseDetails
+        public CourseDetailsController(IApplicationLogger loggingService) : base(loggingService)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View( new CourseDetailsViewModel());
+        }
+    }
+}
---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.CourseModule/Mvc/Models/CourseDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DFC.Digital.Data.Model;
+
+namespace DFC.Digital.Web.Sitefinity.CourseModule
+{
+    public class CourseDetailsViewModel
+    {
+        public CourseDetails CourseDetails { get; set; } = new CourseDetails();
+    }
+}

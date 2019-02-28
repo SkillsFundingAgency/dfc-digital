5319f4fa5fae95250a447a47088f86aff40f161e
vallabha <vallabha24@hotmail.co.uk>
2019-02-28 10:32:08 +0000

    Created controllers and views for Contact Adivser

---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Widgets/Mvc/Controllers/ContactAdviserController.cs
@@ -0,0 +1,70 @@
+﻿using DFC.Digital.Core;
+using DFC.Digital.Web.Core;
+using DFC.Digital.Web.Sitefinity.Core;
+using DFC.Digital.Web.Sitefinity.Widgets.Mvc.Models;
+using System.ComponentModel;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
+
+namespace DFC.Digital.Web.Sitefinity.Widgets.Mvc.Controllers
+{
+    /// <summary>
+    /// Custom Widget for ContactAdviser forms
+    /// </summary>
+    /// <seealso cref="DFC.Digital.Web.Core.BaseDfcController" />
+    [ControllerToolboxItem(Name = "ContactAdviser", Title = "Contact an adviser", SectionName = SitefinityConstants.CustomWidgetSection)]
+
+    public class ContactAdviserController : BaseDfcController
+    {
+        #region Private Fields
+        private ISitefinityCurrentContext sitefinityCurrentContext;
+
+        #endregion Private Fields
+
+        #region Constructors
+        public ContactAdviserController(ISitefinityCurrentContext sitefinityCurrentContext, IApplicationLogger applicationLogger) : base(applicationLogger)
+        {
+            this.sitefinityCurrentContext = sitefinityCurrentContext;
+        }
+
+        #endregion Constructors
+
+        #region Public Properties
+
+        [DisplayName("Your Details Title Text")]
+        public string YourDetailsTitleText { get; set; } = "Your details";
+
+        [DisplayName("Contact Adviser SendTo Email Address")]
+        public string ContactAdvisorSendToEmailAddress { get; set; }
+
+        [DisplayName("Feedback SendTo Email Address")]
+        public string FeedbackSendToEmailAddress { get; set; }
+
+        [DisplayName("Technical Support SendTo Email Address")]
+        public string TechnicalSupportSendToEmailAddress { get; set; }
+
+        #endregion Public Properties
+
+        #region Actions
+
+        // GET: DfcBreadcrumb
+
+        /// <summary>
+        /// entry point to the widget to show a contact form.
+        /// </summary>
+        /// <returns>ActionResult</returns>
+        [HttpGet]
+        [RelativeRoute("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(ContactAdviserViewModel model)
+        {
+            return View();
+        }
+
+        #endregion Actions
+    }
+}
---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Widgets/Mvc/Models/ContactAdviserViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DFC.Digital.Web.Sitefinity.Widgets.Mvc.Models
+{
+    public class ContactAdviserViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Email { get; set; }
+
+        public string ConfirmEmail { get; set; }
+
+        public string ContactOptionType { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string ContactAdivserQuestionType { get; set; }
+
+        public string FeedbackQuestionType { get; set; }
+
+        public bool IsContactable { get; set; }
+
+        public bool TermsAndConditionsChecked { get; set; }
+
+        public string ContactAdvisorSendToEmailAddress { get; set; }
+
+        public string FeedbackSendToEmailAddress { get; set; }
+
+        public string TechnicalSupportSendToEmailAddress { get; set; }
+    }
+}

c90f45fb4e3e979cc26e29a965273420f40e7057
vallabha <vallabha24@hotmail.co.uk>
2019-03-01 16:23:34 +0000

    DFC-7558 Created Configuration repository

---
+++ DFC.Digital/DFC.Digital.Data/Interfaces/IConfigurationRepository.cs
@@ -0,0 +1,15 @@
+﻿using DFC.Digital.Data.Model;
+using System;
+using System.Collections.Generic;
+
+namespace DFC.Digital.Data.Interfaces
+{
+    public interface IConfigurationRepository
+    {
+        Configuration GetByTemplateName(string urlName);
+
+        string GetProviderName();
+
+        Type GetContentType();
+    }
+}
---
+++ DFC.Digital/DFC.Digital.Data/Model/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFC.Digital.Data.Model
+{
+    public class Configuration
+    {
+        public string TemplateName { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}

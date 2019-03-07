0a51af9d9430f17db6fb94f75113ce662d2e2884
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-03-07 09:52:49 +0000

    Started tests for content merge

---
+++ DFC.Digital/DFC.Digital.Services.SendGrid.UnitTests/Services/MergeEmaiContentServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace DFC.Digital.Services.SendGrid.Tests
+{
+    public class MergeEmaiContentServiceTests
+    {
+        [Fact()]
+        public void MergeTemplateBodyWithContentTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}

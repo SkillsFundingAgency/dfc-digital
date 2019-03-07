dcca4b37d1cee378cc8cbe12acd0b24abe903055
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-03-07 11:56:35 +0000

    Updated parameters xml

--- DFC.Digital/DFC.Digital.Services.SendGrid/Services/SendGridClientActions.cs
+++ DFC.Digital/DFC.Digital.Services.SendGrid/Services/SendGridClientActions.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace DFC.Digital.Services
 {
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public class SendGridClientActions : ISendGridClientActions
     {
         public async Task<Response> SendEmailAsync(SendGridClient sendGridClient, SendGridMessage sendGridMessage)

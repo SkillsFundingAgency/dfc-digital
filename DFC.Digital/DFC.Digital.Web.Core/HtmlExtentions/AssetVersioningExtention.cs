39d5877a4bb824010aa38103d20b43a06c57d654
gurney <Gurney@as-tech.co.uk>
2019-03-11 15:36:16 +0000

    Changed views to use CDN

--- DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/AssetVersioningExtention.cs
+++ DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/AssetVersioningExtention.cs
@@ -15,18 +15,17 @@ namespace DFC.Digital.Web.Core
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public static class AssetVersioningExtention
     {
-        private static HttpClient httpClient = new HttpClient();
-
-        public static string VersionAssetFile(this HtmlHelper helper, string fileName)
+       //private static HttpClient httpClient = new HttpClient();
+        public static string GetLocationAssetFileAndVersion(this HtmlHelper helper, string fileName)
         {
             var context = helper?.ViewContext.RequestContext.HttpContext;
             string version = null;
 
             if (context.Cache[fileName] == null)
             {
-                Task<string> getFileHash = GetFileHashAsync(fileName);
-                getFileHash.Wait();
-                version = getFileHash.Result;
+                AsyncHelper asyncHelper = new AsyncHelper();
+
+                version = asyncHelper.Synchronise(() => GetFileHashAsync(fileName));
                 int cacheExpiryMins = Convert.ToInt32(ConfigurationManager.AppSettings[Constants.AssetCacheExpiryTimeMins]);
                 context.Cache.Add(fileName, version, null, DateTime.Now.AddMinutes(cacheExpiryMins), TimeSpan.Zero, CacheItemPriority.Normal, null);
             }
@@ -35,13 +34,19 @@ namespace DFC.Digital.Web.Core
                 version = context.Cache[fileName] as string;
             }
 
-            return $"{fileName}?{version}";
+            return $"{ConfigurationManager.AppSettings[Constants.CDNLocation]}{fileName}?{version}";
+        }
+
+        public static string GetLocationAssetFile(this HtmlHelper helper, string fileName)
+        {
+            return $"{ConfigurationManager.AppSettings[Constants.CDNLocation]}{fileName}";
         }
 
         private static async Task<string> GetFileHashAsync(string fileName)
         {
+            HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri(ConfigurationManager.AppSettings[Constants.CDNLocation]);
-            HttpResponseMessage response = await httpClient.GetAsync("css/govuk-template.min.css");
+            HttpResponseMessage response = await httpClient.GetAsync(fileName);
             if (response.IsSuccessStatusCode)
             {
                 var hashCodes = response.Content.Headers.GetValues("content-md5").FirstOrDefault();

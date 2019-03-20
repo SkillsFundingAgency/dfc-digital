729b00392cf4eb3bac3fc88b6526014849b4777b
vallabha <vallabha24@hotmail.co.uk>
2019-03-20 15:22:39 +0000

    Updated view models with validations

--- DFC.Digital/DFC.Digital.Web.Sitefinity.ContactAdviserModule/Mvc/Models/ViewModels/ContactOptionsViewModel.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.ContactAdviserModule/Mvc/Models/ViewModels/ContactOptionsViewModel.cs
@@ -1,5 +1,4 @@
-﻿using DFC.Digital.Web.Core;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
 {
@@ -7,9 +6,9 @@ namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
     {
         public string Title { get; set; }
 
-        public ContactUsOption ContactUsOption { get; set; }
-
-        //[EnumDataType(typeof(ContactOption), ErrorMessage = "Choose a reason for contacting us")]
-        //public ContactOption ContactOptionType { get; set; }
+        //[Required(ErrorMessage = "Choose a reason for contacting us")]
+        //public ContactUsOption ContactUsOption { get; set; }
+        [EnumDataType(typeof(ContactOption), ErrorMessage = "Choose a reason for contacting us")]
+        public ContactOption ContactOptionType { get; set; }
     }
 }

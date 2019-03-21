dd6274bff9f24cfb3462c1519fa1e3bafe64cbe5
Muthuramana Kandasamy <muthu@lightningcloudconsultancy.com>
2019-03-21 19:05:39 +0000

    Review changes

--- DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/AssetVersioningExtention.cs
+++ DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/AssetVersioningExtention.cs
@@ -35,34 +35,5 @@ namespace DFC.Digital.Web.Core
 
             return context.Cache[fileName].ToString();
         }
-
-        public static string VersionAssetFile(this HtmlHelper helper, string fileName)
-        {
-            var context = helper?.ViewContext.RequestContext.HttpContext;
-            string version = null;
-
-            if (context.Cache[fileName] == null)
-            {
-                var physicalPath = context.Server.MapPath(fileName);
-                version = GetFileHash(physicalPath);
-                int cacheExpiryMins = Convert.ToInt32(ConfigurationManager.AppSettings[Constants.AssetCacheExpiryTimeMins]);
-                context.Cache.Add(fileName, version, null, DateTime.Now.AddMinutes(cacheExpiryMins), TimeSpan.Zero, CacheItemPriority.Normal, null);
-            }
-            else
-            {
-                version = context.Cache[fileName] as string;
-            }
-
-            return $"{fileName}?{version}";
-        }
-
-        private static string GetFileHash(string file)
-        {
-            MD5 md5 = MD5.Create();
-            using (FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Read))
-            {
-                return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
-            }
-        }
     }
 }
--- DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DFC.Digital.Web.Sitefinity.ContactUsModule.MvcApplication" Language="C#" %>

0fbbc90aa359a43653c7d99341227b6d8dc55b0d
Trevor Kapswarah <trevk15@yahoo.co.uk>
2019-03-27 08:11:13 +0000

    View tests and roll back of content title as these are static pages which can be configured in the backend

--- DFC.Digital/DFC.Digital.Web.Sitefinity.Widgets/Mvc/Controllers/SetContentRelatedPageTitleController.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Widgets/Mvc/Controllers/SetContentRelatedPageTitleController.cs
@@ -19,21 +19,19 @@ namespace DFC.Digital.Web.Sitefinity.Widgets.Mvc.Controllers
     {
         #region Private Fields
 
-        private readonly IJobProfileCategoryRepository categoryRepo;
-        private readonly IJobProfileRepository jobProfileRepository;
-        private readonly IWebAppContext webAppContext;
-        private readonly ISitefinityCurrentContext sitefinityCurrentContext;
+        private IJobProfileCategoryRepository categoryRepo;
+        private IJobProfileRepository jobProfileRepository;
+        private IWebAppContext webAppContext;
 
         #endregion Private Fields
 
         #region Constructors
 
-        public SetContentRelatedPageTitleController(IJobProfileCategoryRepository categoryRepo, IJobProfileRepository jobProfileRepo, IWebAppContext webAppContext, ISitefinityCurrentContext sitefinityCurrentContext, IApplicationLogger applicationLogger) : base(applicationLogger)
+        public SetContentRelatedPageTitleController(IJobProfileCategoryRepository categoryRepo, IJobProfileRepository jobProfileRepo, IWebAppContext webAppContext, IApplicationLogger applicationLogger) : base(applicationLogger)
         {
             this.categoryRepo = categoryRepo;
             this.jobProfileRepository = jobProfileRepo;
             this.webAppContext = webAppContext;
-            this.sitefinityCurrentContext = sitefinityCurrentContext;
         }
 
         #endregion Constructors
@@ -70,11 +68,6 @@ namespace DFC.Digital.Web.Sitefinity.Widgets.Mvc.Controllers
             {
                 var searchTerm = HttpUtility.HtmlEncode(webAppContext.RequestQueryString["searchTerm"]);
                 this.ViewBag.Title = $"{searchTerm} {PageTitleSeparator} Search {PageTitleSeparator} {PageTitleSuffix}";
-            }
-            else if (!webAppContext.IsContentAuthoringSite && webAppContext.IsContactUsPage)
-            {
-                var page = sitefinityCurrentContext.CurrentPage;
-                this.ViewBag.Title = $"{page.NavigationNode.Title} {PageTitleSeparator} {PageTitleSuffix}";
             }
 
             //Need to return a blank view so that we can check the view bag in tests.

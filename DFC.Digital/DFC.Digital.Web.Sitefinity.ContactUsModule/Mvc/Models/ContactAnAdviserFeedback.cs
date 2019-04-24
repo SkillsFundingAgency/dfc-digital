eeecc86fc08dd81c4914635c86d99d90335e15a9
vallabha <vallabha24@hotmail.co.uk>
2019-04-24 11:31:29 +0100

    Moved the Custom string trim attribute to core project

--- DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Mvc/Models/ContactAnAdviserFeedback.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Mvc/Models/ContactAnAdviserFeedback.cs
@@ -1,4 +1,4 @@
-﻿using DFC.Digital.Web.Sitefinity.ContactUsModule.Extensions;
+﻿using DFC.Digital.Web.Core;
 using System.ComponentModel.DataAnnotations;
 
 namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
@@ -6,7 +6,7 @@ namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
     public class ContactAnAdviserFeedback
     {
         [Required(ErrorMessage = "Enter a message describing the issue")]
-        [CustomStringLength(1000, ErrorMessage = "Message must be 1000 characters or less")]
+        [TrimCarriageReturnStringLength(1000, ErrorMessage = "Message must be 1000 characters or less")]
         [Display(Name = "Enter your query for the adviser.")]
         public string Message { get; set; }
 
--- DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Mvc/Models/TechnicalFeedback.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Mvc/Models/TechnicalFeedback.cs
@@ -1,4 +1,4 @@
-﻿using DFC.Digital.Web.Sitefinity.ContactUsModule.Extensions;
+﻿using DFC.Digital.Web.Core;
 using System.ComponentModel.DataAnnotations;
 
 namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
@@ -6,7 +6,7 @@ namespace DFC.Digital.Web.Sitefinity.ContactUsModule.Mvc.Models
     public class TechnicalFeedback
     {
         [Required(ErrorMessage = "Enter a message describing the issue")]
-        [CustomStringLength(1000, ErrorMessage = "Message must be 1000 characters or less")]
+        [TrimCarriageReturnStringLength(1000, ErrorMessage = "Message must be 1000 characters or less")]
         [Display(Name = "Include links to the problem page and any page headings. This will help us to fix the issue more quickly.")]
         public string Message { get; set; }
     }

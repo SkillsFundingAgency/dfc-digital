d2a4d58de26113766b1eeb73a76535a23db63606
Ismail Lambat <lambat_15@hotmail.com>
2019-06-12 10:41:10 +0100

    Added comment above EnterText method to advise the reason for using SendKeys method

--- DFC.Digital/DFC.Digital.AcceptanceTest/Infrastructure/Pages/ContactAnAdviserFormsPage.cs
+++ DFC.Digital/DFC.Digital.AcceptanceTest/Infrastructure/Pages/ContactAnAdviserFormsPage.cs
@@ -53,6 +53,9 @@ namespace DFC.Digital.AcceptanceTest.Infrastructure.Pages
         public void EnterText(string id, string query)
         {
             Find.Element(By.Id(id)).Clear();
+
+            //NOTE - Below.SendKeys method will be the new way of interacting with pages / forms / viws rather than using the ViewModels. This is in
+            //preparation for moving away from the current style framework and adopting a framework to use pre-defined Selenium Methods
             Find.Element(By.Id(id)).SendKeys(query);
         }
 

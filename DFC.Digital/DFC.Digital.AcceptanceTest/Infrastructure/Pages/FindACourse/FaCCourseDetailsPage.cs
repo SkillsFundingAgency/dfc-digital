059ae934aaf6e703303f701138e9369e157425dc
Ilambat <Lambat_15@hotmail.com>
2019-06-24 10:48:19 +0100

    DFC 9354 Find a Course Acceptance Tests (#380)
    * Update browser and version numbers for browserstack tests
    
    * Addition of Find a Course tests
    Creation of folders to group pages

---
+++ DFC.Digital/DFC.Digital.AcceptanceTest/Infrastructure/Pages/FindACourse/FaCCourseDetailsPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace DFC.Digital.AcceptanceTest.Infrastructure.Pages
+{
+    public class FaCCourseDetailsPage : DFCPage
+    {
+        public string CourseDetailsTitle => Find.Element(By.ClassName("govuk-heading-l")).Text;
+
+        public bool HasQualificationSection => Find.Element(By.Id("QualificationDetailsLabel")) != null;
+
+        public bool HasCourseDescriptionSection => Find.Element(By.Id("CourseDescriptionLabel")) != null;
+
+        public bool HasEntryRequirementSection => Find.Element(By.Id("EntryRequirementsLabel")) != null;
+
+        public bool HasEquipmentRequiredSection => Find.Element(By.Id("EquipmentRequiredLabel")) != null;
+
+        public bool HasAssessmentMethodSection => Find.Element(By.Id("AssessmentMethodLabel")) != null;
+
+        public bool HasVenueSection => Find.Element(By.Id("VenueLabel")) != null;
+
+        public bool HasOtherDatesAndVenuesSection => Find.Element(By.Id("OtherDatesAndVenuesLabel")) != null;
+    }
+}

793218cfc02fbeb2f967411e0982d40aabf8f03b
Ismail Lambat <lambat_15@hotmail.com>
2019-06-24 09:41:50 +0100

    Addition of Find a Course tests Creation of folders to group pages

--- DFC.Digital/DFC.Digital.AcceptanceTest/Infrastructure/Config/DFCPage.cs
+++ DFC.Digital/DFC.Digital.AcceptanceTest/Infrastructure/Config/DFCPage.cs
@@ -8,6 +8,15 @@ namespace DFC.Digital.AcceptanceTest.Infrastructure
     {
         protected new IElementFinder Find => new DfcElementFinder(base.Find, Execute, WaitFor, Browser);
 
+        public void EnterText(string id, string query)
+        {
+            Find.Element(By.Id(id)).Clear();
+
+            //NOTE - Below.SendKeys method will be the new way of interacting with pages / forms / viws rather than using the ViewModels. This is in
+            //preparation for moving away from the current style framework and adopting a framework to use pre-defined Selenium Methods
+            Find.Element(By.Id(id)).SendKeys(query);
+        }
+
         protected virtual TPage NavigateTo<TPage>(By by, int waitTimeout = 10)
     where TPage : UiComponent, new()
         {

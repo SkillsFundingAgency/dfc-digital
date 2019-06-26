ac0de64f70ffe98cfa50d0a7c5ee22bf42f669ac
vallabha <vallabha24@hotmail.co.uk>
2019-06-26 15:38:53 +0100

    Added functionality to remove the meta generator

--- DFC.Digital/DFC.Digital.Web.Core/HttpModules/CustomHeaderModule.cs
+++ DFC.Digital/DFC.Digital.Web.Core/HttpModules/CustomHeaderModule.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
 
 namespace DFC.Digital.Web.Core
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public class CustomHeaderModule : IHttpModule
     {
-        private const string MetaAttrKey = "name";
-        private const string MetaAttrValue = "Generator";
-
         public void Init(HttpApplication context)
         {
             if (context != null)
@@ -26,37 +21,9 @@ namespace DFC.Digital.Web.Core
         private void OnPreSendRequestHeaders(object sender, EventArgs e)
         {
             HttpContext.Current?.Response.Headers.Remove("Server");
-            HttpContext oContext = ((HttpApplication)sender).Context;
-            if (oContext.Handler is Page)
-            {
-                Page objPage = (Page)oContext.Handler;
-                objPage.PreRender += new EventHandler(ObjPage_PreRender);
-            }
 
             // Or you can set something misleading
             //HttpContext.Current.Response.Headers.Set("Server", "NCS Server");
-        }
-
-        private void ObjPage_PreRender(object sender, EventArgs e)
-        {
-            HttpContext oContext = ((HttpApplication)sender).Context; // HttpContext.Current.CurrentHandler as Page;
-
-            //throw new NotImplementedException();
-            if (oContext.Handler is Page page)
-            {
-                var headerControls = page.Header.Controls;
-                foreach (var control in headerControls)
-                {
-                    if (control is HtmlMeta metaTag)
-                    {
-                        if (metaTag.Attributes[MetaAttrKey] == MetaAttrValue)
-                        {
-                            headerControls.Remove(metaTag);
-                            break;
-                        }
-                    }
-                }
-            }
         }
     }
 }

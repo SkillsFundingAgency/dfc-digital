e521ff77a749744b24568737438959290559b4f8
gurney <Gurney@as-tech.co.uk>
2019-08-13 09:46:09 +0100

    Fix spelling mistake.

--- DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/CompositeServiceExtensions.cs
+++ DFC.Digital/DFC.Digital.Web.Core/HtmlExtentions/CompositeServiceExtensions.cs
@@ -5,7 +5,7 @@ using System.Web.Mvc;
 namespace DFC.Digital.Web.Core
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    public static class CompositeServiceExtentions
+    public static class CompositeServiceExtensions
     {
         public static string GetCompositeServiceEndPoint(this HtmlHelper helper)
         {

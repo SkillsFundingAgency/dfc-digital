0c31780b31daecdcad2aa7393b8673ce75275e97
vallabha24 <32341949+vallabha24@users.noreply.github.com>
2019-09-25 12:36:45 +0100

    DFC-9860 sitefinity upgrade analysis (#404)
    Upgraded Sitefinity to version 12.1

--- DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Properties/AssemblyInfo.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.ContactUsModule/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DFC.Digital.Web.Sitefinity.ContactUsModule")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright �  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

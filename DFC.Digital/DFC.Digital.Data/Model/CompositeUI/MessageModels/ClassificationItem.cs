21db285f42a06267e8f2d6b2609a6916eb45a44f
vallabha24 <32341949+vallabha24@users.noreply.github.com>
2019-11-14 16:20:44 +0000

    Resolved issue with Url value not showing for other classifications (#441)

--- DFC.Digital/DFC.Digital.Data/Model/CompositeUI/MessageModels/ClassificationItem.cs
+++ DFC.Digital/DFC.Digital.Data/Model/CompositeUI/MessageModels/ClassificationItem.cs
@@ -9,7 +9,5 @@ namespace DFC.Digital.Data.Model
     public class ClassificationItem : RelatedContentItem
     {
         public string Url { get; set; }
-
-        public string Description { get; set; }
     }
 }

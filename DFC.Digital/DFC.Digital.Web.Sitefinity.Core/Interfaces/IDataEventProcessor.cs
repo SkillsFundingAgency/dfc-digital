860cec18085765532d3061fb2bf73553a9cffd97
Gurnaik-Randhawa <Gurnaik-Randhawa@users.noreply.github.com>
2019-11-20 08:41:48 +0000

    Fixed so that when a delete happens for classsification, don't try and export as it was throwing exception because the item is already deleted. (#446)

--- DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Interfaces/IDataEventProcessor.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.Core/Interfaces/IDataEventProcessor.cs
@@ -9,6 +9,8 @@ namespace DFC.Digital.Web.Sitefinity.Core
     {
         void ExportCompositePage(IDataEvent eventInfo);
 
+        void ExportContentData(IDataEvent eventInfo);
+
         void PublishDynamicContent(DynamicContent eventInfo);
     }
 }

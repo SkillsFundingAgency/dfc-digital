e341538d5a2c967de13fcb4e94818558cb105a1d
gurney <Gurney@as-tech.co.uk>
2020-01-10 12:02:05 +0000

    Added views, service and tests to handle workbased venues

---
+++ DFC.Digital/DFC.Digital.Data/Model/CourseRegion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DFC.Digital.Data.Model
+{
+    public class CourseRegion
+    {
+        public string Region { get; set; }
+
+        public string Area { get; set; }
+    }
+}

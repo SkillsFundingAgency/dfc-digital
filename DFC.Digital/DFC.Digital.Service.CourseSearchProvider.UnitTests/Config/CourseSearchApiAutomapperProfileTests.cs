dfceebf3360d5ab7fdbc609fba32ef298371d32f
gurney <Gurney@as-tech.co.uk>
2020-01-16 11:44:00 +0000

    Fix failing tests

--- DFC.Digital/DFC.Digital.Service.CourseSearchProvider.UnitTests/Config/CourseSearchApiAutomapperProfileTests.cs
+++ DFC.Digital/DFC.Digital.Service.CourseSearchProvider.UnitTests/Config/CourseSearchApiAutomapperProfileTests.cs
@@ -1,15 +1,7 @@
 ﻿using AutoMapper;
-using DFC.Digital.Core;
-using DFC.Digital.Data.Interfaces;
 using DFC.Digital.Data.Model;
-using DFC.Digital.Service.CourseSearchProvider.CourseSearchServiceApi;
-using FakeItEasy;
 using FluentAssertions;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
 using Xunit;
 using FAC = DFC.FindACourseClient;
 
@@ -17,10 +9,15 @@ namespace DFC.Digital.Service.CourseSearchProvider.UnitTests
 {
     public class CourseSearchApiAutomapperProfileTests
     {
+        private readonly FAC.CourseDetails clientCourseDetails;
+
         public CourseSearchApiAutomapperProfileTests()
         {
             Mapper.Reset();
             Mapper.Initialize(m => m.AddProfile<CourseSearchApiAutomapperProfile>());
+            clientCourseDetails = new FAC.CourseDetails();
+            clientCourseDetails.Title = nameof(FAC.CourseDetails.Title);
+            clientCourseDetails.SubRegions = new List<FAC.SubRegion>();
         }
 
         [Theory]
@@ -29,7 +26,6 @@ namespace DFC.Digital.Service.CourseSearchProvider.UnitTests
         [InlineData("1.23", "£1.23")]
         public void AutoMapperProfileCourseCostsConverter(string inCost, string expectedFormatedCost)
         {
-            var clientCourseDetails = new FAC.CourseDetails();
             clientCourseDetails.Cost = inCost;
 
             var mapped = Mapper.Map<CourseDetails>(clientCourseDetails);
@@ -40,12 +36,7 @@ namespace DFC.Digital.Service.CourseSearchProvider.UnitTests
         [Fact]
         public void AutoMapperProfileCourseRegionsConverter()
         {
-            Mapper.Initialize(m => m.AddProfile<CourseSearchApiAutomapperProfile>());
-            var clientCourseDetails = new FAC.CourseDetails();
-            clientCourseDetails.Title = nameof(FAC.CourseDetails.Title);
-            clientCourseDetails.SubRegions = new List<FAC.SubRegion>();
-
-            var expectedVenuesOutput = new List<CourseRegion>();
+             var expectedVenuesOutput = new List<CourseRegion>();
 
             //Build the input and expected output
             for (int regions = 0; regions < 2; regions++)

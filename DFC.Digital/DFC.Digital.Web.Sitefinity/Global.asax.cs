54d8e50d28da999e581c0f6cdd4e2f0771bd8205
vallabha <vallabha24@gmail.com>
2020-02-04 12:04:11 +0000

    DFC-9023-Added Jquery fix

---
+++ DFC.Digital/DFC.Digital.Web.Sitefinity/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace DFC.Digital.Web.Sitefinity
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.Url.AbsoluteUri.Contains("jquery-1.11.0.min.js"))
+            {
+                HttpContext.Current.Response.Redirect($"{ConfigurationManager.AppSettings["DFC.Digital.CDNLocation"]}/gds_service_toolkit/js/jquerybundle.min.js", true);
+            }
+        }
+    }
+}

f1a930b540b87f29f4da604894e122992fe796a2
vallabha <vallabha24@gmail.com>
2020-05-07 11:59:27 +0100

    DFC-12554-removed empty space

--- DFC.Digital/DFC.Digital.Web.Sitefinity.WebApi/Properties/AssemblyInfo.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.WebApi/Properties/AssemblyInfo.cs
@@ -33,8 +33,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-
-
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
 
 [assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]

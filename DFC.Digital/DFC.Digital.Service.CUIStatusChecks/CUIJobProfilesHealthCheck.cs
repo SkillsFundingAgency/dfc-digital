79ad332a9a898da5a9bb6352e4aa72aaa9473000
gurney <Gurney@as-tech.co.uk>
2020-07-17 14:06:08 +0100

    Refactored to use comon class for all CUI checks

--- DFC.Digital/DFC.Digital.Service.CUIStatusChecks/CUIJobProfilesHealthCheck.cs
+++ DFC.Digital/DFC.Digital.Service.CUIStatusChecks/CUIJobProfilesHealthCheck.cs
@@ -1,6 +1,4 @@
-﻿
-
-using DFC.Digital.Core;
+﻿using DFC.Digital.Core;
 using DFC.Digital.Data.Interfaces;
 using DFC.Digital.Data.Model;
 using Newtonsoft.Json;
@@ -13,16 +11,14 @@ namespace DFC.Digital.Service.CUIStatusChecks
 {
     public class CUIJobprofilesHealthCheck : IServiceStatus
     {
-        private readonly IApplicationLogger applicationLogger;
+        private readonly IServiceStatusCUIApp serviceStatusCUIApp;
         private readonly IConfigurationProvider configurationProvider;
-        private readonly IHttpClientService<IServiceStatus> httpClient;
 
         #region ctor
-        public CUIJobprofilesHealthCheck(IApplicationLogger applicationLogger, IConfigurationProvider configurationProvider, IHttpClientService<IServiceStatus> httpPublishingClient)
+        public CUIJobprofilesHealthCheck(IConfigurationProvider configurationProvider, IServiceStatusCUIApp serviceStatusCUIApp)
         {
-            this.applicationLogger = applicationLogger;
-            this.httpClient = httpPublishingClient;
             this.configurationProvider = configurationProvider;
+            this.serviceStatusCUIApp = serviceStatusCUIApp;
         }
         #endregion
 
@@ -31,41 +27,7 @@ namespace DFC.Digital.Service.CUIStatusChecks
 
         public async Task<ServiceStatus> GetCurrentStatusAsync()
         {
-            var serviceStatus = new ServiceStatus { Name = ServiceName, Status = ServiceState.Red, CheckCorrelationId = Guid.NewGuid() };
-
-            try
-            {
-                this.httpClient.AddHeader("Accept", "application/json");
-                var response = await this.httpClient.GetAsync(configurationProvider.GetConfig<string>(Constants.CUIAppJobProfilesHealthEndPoint));
-
-                if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    serviceStatus.Status = ServiceState.Amber;
-                    var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    var cuiHealthStatuses = JsonConvert.DeserializeObject<IList<ServiceStatusCUIResponse>>(responseString);
-                    serviceStatus.ChildAppStatuses = new List<ServiceStatusChildApp>();
-                    foreach (ServiceStatusCUIResponse s in cuiHealthStatuses)
-                    {
-                        serviceStatus.ChildAppStatuses.Add(new ServiceStatusChildApp() { Name = s.Service, Status = s.Message.Contains("is available") ? ServiceState.Green : ServiceState.Red });
-                    }
-
-                    if (serviceStatus.ChildAppStatuses.All(s => s.Status == ServiceState.Green))
-                    {
-                        serviceStatus.Status = ServiceState.Green;
-                        serviceStatus.CheckCorrelationId = Guid.Empty;
-                    }
-                }
-                else
-                {
-                    applicationLogger.Warn($"{nameof(CUIJobprofilesHealthCheck)}.{nameof(GetCurrentStatusAsync)} : {Constants.ServiceStatusWarnLogMessage} - Correlation Id [{serviceStatus.CheckCorrelationId}] - Non Success Response StatusCode [{response.StatusCode}] Reason [{response.ReasonPhrase}]");
-                }
-            }
-            catch (Exception ex)
-            {
-                applicationLogger.ErrorJustLogIt($"{nameof(CUIJobprofilesHealthCheck)}.{nameof(GetCurrentStatusAsync)} : {Constants.ServiceStatusFailedLogMessage} - Correlation Id [{serviceStatus.CheckCorrelationId}]", ex);
-            }
-
-            return serviceStatus;
+            return await serviceStatusCUIApp.GetCurrentCUIAppStatusAsync(ServiceName, configurationProvider.GetConfig<string>(Constants.CUIAppJobProfilesHealthEndPoint));
         }
         #endregion
 

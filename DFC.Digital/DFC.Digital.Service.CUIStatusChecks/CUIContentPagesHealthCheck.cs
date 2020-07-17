ced013330be6db9813b798f7e5b2800ceb6c5e49
gurney <Gurney@as-tech.co.uk>
2020-07-17 14:13:32 +0100

    Added in content pages

--- DFC.Digital/DFC.Digital.Service.CUIStatusChecks/CUIContentPagesHealthCheck.cs
+++ DFC.Digital/DFC.Digital.Service.CUIStatusChecks/CUIContentPagesHealthCheck.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace DFC.Digital.Service.CUIStatusChecks
 {
-    public class CUIJobprofilesHealthCheck : IServiceStatus
+    public class CUIContentPagesHealthCheck : IServiceStatus
     {
         private readonly IServiceStatusCUIApp serviceStatusCUIApp;
         private readonly IConfigurationProvider configurationProvider;
 
         #region ctor
-        public CUIJobprofilesHealthCheck(IConfigurationProvider configurationProvider, IServiceStatusCUIApp serviceStatusCUIApp)
+        public CUIContentPagesHealthCheck(IConfigurationProvider configurationProvider, IServiceStatusCUIApp serviceStatusCUIApp)
         {
             this.configurationProvider = configurationProvider;
             this.serviceStatusCUIApp = serviceStatusCUIApp;
@@ -23,11 +23,11 @@ namespace DFC.Digital.Service.CUIStatusChecks
         #endregion
 
         #region Implement of IServiceStatus
-        private static string ServiceName => "CUI: Job profiles";
+        private static string ServiceName => "CUI: Content pages";
 
         public async Task<ServiceStatus> GetCurrentStatusAsync()
         {
-            return await serviceStatusCUIApp.GetCurrentCUIAppStatusAsync(ServiceName, configurationProvider.GetConfig<string>(Constants.CUIAppJobProfilesHealthEndPoint));
+            return await serviceStatusCUIApp.GetCurrentCUIAppStatusAsync(ServiceName, configurationProvider.GetConfig<string>(Constants.CUIAppContentPagesHealthEndPoint));
         }
         #endregion
 

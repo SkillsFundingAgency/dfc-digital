8f7c1f127b80bcce8b00c2e896fae400981e46df
Muthuramana Kandasamy <muthu@lightningcloudconsultancy.com>
2020-10-22 21:20:37 +0100

    conflict resolution

--- DFC.Digital/DFC.Digital.Web.Sitefinity.JobProfileModule/Config/JobProfilesAutoMapperProfile.cs
+++ DFC.Digital/DFC.Digital.Web.Sitefinity.JobProfileModule/Config/JobProfilesAutoMapperProfile.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using DFC.Digital.Data.Model;
-using DFC.Digital.Web.Sitefinity.JobProfileModule.Config;
 using DFC.Digital.Web.Sitefinity.JobProfileModule.Mvc.Controllers;
 using DFC.Digital.Web.Sitefinity.JobProfileModule.Mvc.Models;
 using System.Globalization;

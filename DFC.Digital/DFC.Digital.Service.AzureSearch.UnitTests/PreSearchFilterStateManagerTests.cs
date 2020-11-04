9728cf2c2ae3660ef6eefd5048ee6c00c463ff24
vallabha <vallabha24@gmail.com>
2020-11-04 12:31:35 +0000

    Fixed PSF UNIT tests

--- DFC.Digital/DFC.Digital.Service.AzureSearch.UnitTests/PreSearchFilterStateManagerTests.cs
+++ DFC.Digital/DFC.Digital.Service.AzureSearch.UnitTests/PreSearchFilterStateManagerTests.cs
@@ -123,8 +123,8 @@ namespace DFC.Digital.Service.AzureSearch.UnitTests
         }
 
         [Theory]
-        [InlineData(true, false, "{\"Sections\":[{\"Name\":null,\"SectionDataType\":\"Interest\",\"Options\":[{\"IsSelected\":true,\"ClearOtherOptionsIfSelected\":false,\"OptionKey\":\"SingleSelectedValue\"}],\"SingleSelectedValue\":\"\"}]}")]
-        [InlineData(true, true, "{\"Sections\":[{\"Name\":null,\"SectionDataType\":\"Interest\",\"Options\":[{\"IsSelected\":true,\"ClearOtherOptionsIfSelected\":false,\"OptionKey\":\"SingleSelectedValue\"}],\"SingleSelectedValue\":\"SingleSelectedValue\"}]}")]
+        [InlineData(true, false, "{\"Sections\":[{\"Name\":null,\"SectionDataType\":\"Interest\",\"Options\":[{\"IsSelected\":true,\"ClearOtherOptionsIfSelected\":false,\"OptionKey\":\"SingleSelectedValue\",\"PSFCategory\":null}],\"SingleSelectedValue\":\"\"}]}")]
+        [InlineData(true, true, "{\"Sections\":[{\"Name\":null,\"SectionDataType\":\"Interest\",\"Options\":[{\"IsSelected\":true,\"ClearOtherOptionsIfSelected\":false,\"OptionKey\":\"SingleSelectedValue\",\"PSFCategory\":null}],\"SingleSelectedValue\":\"SingleSelectedValue\"}]}")]
         [InlineData(false, false, "{\"Sections\":[]}")]
         public void SaveStateTest(bool validPreviousSection, bool singleValue, string expectedJson)
         {

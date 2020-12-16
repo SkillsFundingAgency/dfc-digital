952b255209c288f99140dd66624f3dc487b93290
vallabha <vallabha24@gmail.com>
2020-12-16 09:31:35 +0000

    DFC-13444 Resolved issue related to SOCCODE

--- DFC.Digital/DFC.Digital.Repository.SitefinityCMS/Extensions/DynamicContentExtensions.cs
+++ DFC.Digital/DFC.Digital.Repository.SitefinityCMS/Extensions/DynamicContentExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using DFC.Digital.Core;
 using Telerik.Sitefinity.DynamicModules.Model;
+using Telerik.Sitefinity.GenericContent.Model;
 
 namespace DFC.Digital.Repository.SitefinityCMS.Extensions
 {
@@ -8,6 +9,11 @@ namespace DFC.Digital.Repository.SitefinityCMS.Extensions
         public static string GetContentItemIdKey(this DynamicContent content)
         {
             return content.ApprovalWorkflowState == Constants.WorkflowStatusDraft ? Constants.ContentId : Constants.OriginalContentId;
+        }
+
+        public static string GetAdditionalContentItemIdKey(this DynamicContent content)
+        {
+            return content.Status == ContentLifecycleStatus.Live ? Constants.OriginalContentId : Constants.ContentId;
         }
     }
 }
